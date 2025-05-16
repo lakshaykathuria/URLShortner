2d640d95624ae7cf7888ab04a565fd6ff3c6d339
lakshaykathuria <lakshayakathuria2003@gmail.com>
2025-05-16 21:50:39 +0530

    frontend

--- src/main/java/com/shortner/URLShortner/controller/HomeController.java
+++ src/main/java/com/shortner/URLShortner/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-    @RequestMapping(value = { "/", "/{path:^(?!shorten$).*$}" })
+    @RequestMapping(value = { "/", "/{path:^(?!shorten$).*$}"})
     public String index() {
         // Serve the frontend UI
         return "index.html";
